eba197248f87f97e70c9081363ce37419c348d04
FedeFunes <federicofunes92@gmail.com>
2016-06-23 20:08:22 -0300

    Primera Parte 1b

---
+++ nutricloud-web/Models/Index.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nutricloud_web.Models
+{
+    public class LogInViewModel
+    {
+    }
+
+    public class SignInViewModel
+    {
+    }
+}
